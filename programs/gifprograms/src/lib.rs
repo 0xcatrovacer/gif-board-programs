31278efb402495f3b66d261c4acde8a799a1f877
Swarnab Garang <swarnabgarang@gmail.com>
2022-02-11 11:59:29 +0530

    added function to add gifs and added tests

--- programs/gifprograms/src/lib.rs
+++ programs/gifprograms/src/lib.rs
@@ -10,6 +10,12 @@ pub mod gifprograms {
         base_account.total_gifs = 0;
         Ok(())
     }
+
+    pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -19,6 +25,12 @@ pub struct Initialize<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
 }
 
 
